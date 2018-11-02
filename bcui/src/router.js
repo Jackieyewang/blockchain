4ec3e298d01479dea08f6b9f2bc5aa8044b616d5
HenryGuo <guohongyi@sjtu.edu.cn>
2018-11-02 08:13:08 +0800

    solve conflict

--- bcui/src/router.js
+++ bcui/src/router.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React,{Component} from 'react';
 import {HashRouter,Route,Switch} from 'react-router-dom';
 import DefaultLayout from './layouts/DefaultLayout/DefaultLayout';
@@ -19,26 +18,4 @@ export default class RouterWrap extends Component{
             </div>
         )
     }
-=======
-import React,{Component} from 'react';
-import {HashRouter,Route,Switch} from 'react-router-dom';
-import DefaultLayout from './layouts/DefaultLayout/DefaultLayout';
-import LoginUser from './layouts/LoginUser/LoginUser';
-import Register from './routes/Register/Register';
-
-export default class RouterWrap extends Component{
-    render(){
-        return (
-            <div id="router">
-                <HashRouter>
-                    <Switch>
-                        <Route path="/login" component={LoginUser}  exact />
-                        <Route path="/register" component={Register}  exact />
-                        <Route path="/home" component={DefaultLayout} />
-                    </Switch>
-                </HashRouter>
-            </div>
-        )
-    }
->>>>>>> c30ae2813737accacd29b8908585d0c098d59104
 }
